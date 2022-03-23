addffcc2671e7bc7bd437b6e8caf9a1f0d1ace2e
ilia <ilia.rodkin@gmail.com>
2022-03-23 18:05:23 +0300

    Initial commit

---
+++ shared/src/commonMain/kotlin/by/candy/suharnica/Greeting.kt
@@ -0,0 +1,7 @@
+package by.candy.suharnica
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/by/candy/suharnica/Platform.kt
@@ -0,0 +1,5 @@
+package by.candy.suharnica
+
+expect class Platform() {
+    val platform: String
+}
